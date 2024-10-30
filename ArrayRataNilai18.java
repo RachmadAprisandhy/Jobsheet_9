f3a2642ea848c541b401a6824538b3450d53fafa
Rachmad Aprisandy <rachmadblackspade212@gmail.com>
2024-10-30 10:57:14 +0700

    commit4jb9

--- ArrayRataNilai18.java
+++ ArrayRataNilai18.java
@@ -3,19 +3,32 @@ import java.util.Scanner;
 public class ArrayRataNilai18{
     public static void main (String [] args){
         Scanner sc = new Scanner(System.in);
-        int [] NilaiMhs = new int[10];
+        int mahasiswa,mahasiswalulus = 0,mahasiswatidaklulus = 0;
         double total = 0;
-        double rata;
+        double rata=0,ratatidaklulus = 0,ratalulus = 0,totallulus = 0,totaltidaklulus = 0;
+        
+
+
+        System.out.print("Masukkan Jumlah Mahasiswa : ");
+        mahasiswa = sc.nextInt();
+
+        int [] NilaiMhs = new int[mahasiswa];
 
         for(int i = 0; i <NilaiMhs.length; i++){
             System.out.print("masukkan nilai mahasiswa ke - " + (i+1) + " : ");
             NilaiMhs [i] = sc.nextInt();
+            if (NilaiMhs[i] > 70){
+                mahasiswalulus ++;
+                totallulus += NilaiMhs[i];
+            }if(NilaiMhs [i]<= 70){
+                mahasiswatidaklulus ++;
+                totaltidaklulus += NilaiMhs[i];
+            }
         }
-        for (int i = 0; i< NilaiMhs.length; i++){
-            total += NilaiMhs [i];
-        }
-        rata = total /NilaiMhs.length;
-        System.out.println("Rata-rata Nilai Mahasiswa = " + rata);
+        ratalulus = totallulus / mahasiswalulus;
+        ratatidaklulus = totaltidaklulus / mahasiswatidaklulus;
+        System.out.println("Rata-rata Nilai Mahasiswa lulus = " + ratalulus);
+        System.out.println("Rata-rata Nilai Mahasiswa tidak lulus= " + ratatidaklulus);
 
     }
 }
