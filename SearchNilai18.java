f1fcdb70e62e5f5c9c845e2d6b72654959b4d2ac
Rachmad Aprisandy <rachmadblackspade212@gmail.com>
2024-10-30 11:40:27 +0700

    commit6jb9

--- SearchNilai18.java
+++ SearchNilai18.java
@@ -1,19 +1,35 @@
-
+import java.util.Scanner;
 public class SearchNilai18{
     public static void main (String [] args){
-        int[] arrNilai = {80,85,78,96,90,82,86};
-        int key= 90;
+        Scanner sc = new Scanner( System.in);
+        int key = 0;
         int hasil = 0;
+        int jumlahNilai;
+
+        System.out.print("Masukkan Jumlah Mahasiswa : ");
+         jumlahNilai = sc.nextInt();
+
+         int [] arrNilai = new int [jumlahNilai]; 
         
+        for(int i= 0; i < arrNilai.length; i++){
+            System.out.print("masukkan nilai mahasiswa ke - " + (i+1) + ":");
+            arrNilai [i] = sc.nextInt();
+        }
+        System.out.println(" masukkan nilai yang akan anda cari");
+        key = sc.nextInt();
         for (int i =0; i < arrNilai.length;i++){
-            if (key == arrNilai[i]){
+            if ( arrNilai[i] == key){
                 hasil = i;
                 break;
             }
         }
-        System.out.println();
-        System.out.println("nilai " + key + " ketemu pada indeks ke -" + hasil);
-        System.out.println();
+        if (hasil !=0){
+            System.out.println("nilia yang anda cari" + key + ", merupakan nilai dari mahasiswa ke- : " + (hasil + 1));
+        }else{5
+        
+            System.out.println("nilai yang anda cari tidak ditemukan");
+        }
+
 
 
     }
